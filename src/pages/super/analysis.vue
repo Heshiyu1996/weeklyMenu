7eed21f8e0eabaec0da97016232e5df3a434146e
你的宇哥哥 <450979309@qq.com>
2018-03-19 22:09:01 +0800

    feature：第一个分析

--- src/pages/super/analysis.vue
+++ src/pages/super/analysis.vue
@@ -1,20 +1,44 @@
 <template>
     <div>
-        <div class="reply">
-            s
+        <div class="analysis">
+            <schart
+                class="prov"
+                :canvasId="canvasId"
+                :type="type"
+                :width="width"
+                :height="height"
+                :data="provData"
+                :options="options"
+            ></schart>
         </div>
     </div>
 </template>
 
 <script>
 import { prefix } from '@/publicAPI/config'
+import Schart from 'vue-schart';
 
 export default {
     name: 'Analysis',
     data () {
         return {
+            canvasId: 'prov',
+            type: 'bar',
+            width: 375,
+            height: 300,
+            provData: [],
+            options: {
+                padding: 30,
+                title: '员工的来源分布',
+                titlePosition: 'bottom'
+            }
         }
     },
+
+    components:{
+        Schart
+    },
+
     computed: {
         userInfo () {
             return this.$store.getters.getUserInfo
@@ -22,9 +46,42 @@ export default {
     },
 
     methods: {
+        mySort (property) {
+            return function(a,b){
+                var value1 = a[property];
+                var value2 = b[property];
+                return value2 - value1;
+            }
+        },
+
+        getAnalysisByProvince () {
+            let that = this
+            this.$axios.get(`${prefix}/admin/getAnalysisByProvince`)
+            .then((res) => {
+                if (res.data.success) {
+                    this.provData = [...res.data.relatedObject]
+                    console.log(this.provData.sort(this.mySort('value')))
+                    let obj = {
+                        name: '其他',
+                        value: 0
+                    }
+                    for (let i=this.provData.length / 2; i<this.provData.length; i++) {
+                        obj.value += this.provData[i].value
+                    }
+                    let middle = this.provData.length / 2
+                    this.provData = this.provData.slice(0, middle)
+                    // this.provData[middle] = obj
+                    console.log(this.provData)
+                }
+            })
+            .catch((err) => {
+                alert(err)
+            })
+        }
     },
-    
+
     mounted () {
+        this.getAnalysisByProvince()
     }
 }
 </script>
@@ -32,9 +89,21 @@ export default {
 <style lang="postcss" type="text/css" rel="stylesheet/css" scoped>
 @import "../../common.css";
 
-.reply {
+.analysis {
     position: absolute;
     width: 100%;
     background: $gray1;
+
+    .prov {
+        width: px2rem(340px);
+        height: px2rem(290px);
+        margin: 0 auto;
+        padding: px2rem(10px) 0;
+        text-align: center;
+
+        #prov {
+            width: 100% !important;
+        }
+    }
 }
 </style>
